c598bd45b17b51e47ba4cac0d93f49e674102b98
HP <HP@192.168.1.6>
2025-08-02 09:56:26 +0530

    initial commit

---
+++ java_pr/src/java_pr/GetPropertiesDemo.java
@@ -0,0 +1,9 @@
+package java_pr;
+
+import java.util.*;
+class GetPropertiesDemo {
+public static void main (String [] args) {
+Properties x = System. getProperties () ;
+x. list (System. out) ;
+}
+}
